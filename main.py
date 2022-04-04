3d814b4da7d4efdf449341032a040d2c3e511943
RokerDev <rokerdev91@gmail.com>
2022-04-04 21:46:36 +0200

    feat: create app design

--- main.py
+++ main.py
@@ -1,2 +1,45 @@
 import tkinter as tk
+from tkinter import scrolledtext
 
+
+class DisappearingTextWritingApp(tk.Tk):
+
+    def __init__(self):
+        super().__init__()
+
+        self.title("Disappearing Text Writing App")
+        self.geometry("600x400")
+
+        self.user_input_string = tk.StringVar()
+        self.bind("<KeyPress>", self.key_press)
+        self.bind("<KeyRelease>", self.key_release)
+
+        self.main_frame = tk.Frame(self)
+        self.main_frame.pack()
+
+        # Multiline space for user input
+        self.label = tk.Label(
+            master=self.main_frame,
+            text="Type your text here",
+            font=("Times New Roman", 25)
+        )
+        self.label.grid(
+            column=0,
+            row=0,
+            pady=(10, 10),
+            sticky=tk.EW
+        )
+
+        self.text_area = scrolledtext.ScrolledText(
+            master=self.main_frame,
+            wrap=tk.WORD,
+            width=50,
+            height=14,
+            font=("Times New Roman", 15)
+        )
+        self.text_area.grid(
+            column=0,
+            row=1,
+            pady=(10, 0),
+            sticky=tk.EW
+        )
